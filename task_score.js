6070df30cae0bba326aaf682ffa828f6033cc89c
luba17 <lm@crosslife.me>
2022-10-30 17:34:55 +0300

    added average point

---
+++ task_score.js
@@ -0,0 +1,10 @@
+// Составить расчет среднего балла за домашние задания студента по модулям: введение, git, js
+let introduction = 100;
+let git = 130;
+let js = 150;
+let averagePoint = (introduction + git + js)/3;
+
+let userName = "Luba";
+let surname = "Muratova";
+
+console.log(`Average point for all hometask by ${userName} ${surname}: ${averagePoint}`);
